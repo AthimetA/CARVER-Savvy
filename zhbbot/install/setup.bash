3a6118132e90f90817671a7d37363523884db18f
ValDeus1151 <sirawich.kmutt@mail.kmutt.ac.th>
2024-02-19 16:45:26 +0700

    Service added

--- zhbbot/install/setup.bash
+++ zhbbot/install/setup.bash
@@ -22,7 +22,7 @@ _colcon_prefix_chain_bash_source_script() {
 COLCON_CURRENT_PREFIX="/opt/ros/humble"
 _colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash"
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
-COLCON_CURRENT_PREFIX="/home/athimet/FRA532_Mobile_Robot/cocoam/install"
+COLCON_CURRENT_PREFIX="/home/duplicix/dev_ws/install"
 _colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash"
 
 # source this prefix
