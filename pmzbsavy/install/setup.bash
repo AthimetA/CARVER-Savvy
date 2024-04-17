5e47e57ae45a37716e2eaaf0d80cd84ecd0a834a
ValDeus <sirawich.kmutt@mail.kmutt.ac.th>
2024-04-18 03:06:34 +0700

    Update pmzbsavy

--- pmzbsavy/install/setup.bash
+++ pmzbsavy/install/setup.bash
@@ -24,6 +24,9 @@ _colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
 COLCON_CURRENT_PREFIX="/home/nuc11/ros2_ws/install"
 _colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash"
+# setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
+COLCON_CURRENT_PREFIX="/home/nuc11/CARVER-Savvy/zhbbot/install"
+_colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash"
 
 # source this prefix
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
